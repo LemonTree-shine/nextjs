3cbe361ae788ba0d2a216552bb45281cb7633001
陈泽 <18815288453@163.com>
2019-04-21 21:58:42 +0800

    发布文章接口

--- pages/books.jsx
+++ pages/books.jsx
@@ -1,15 +1,16 @@
 import {Component} from "react";
-import "../style/article.less"
+import "../style/article.less";
+import axios from "axios";
 export default class Index extends Component{
     render(){
         return <div className="c-write-article">
             <div className="article-title">
-                <input className="title-input" placeholder="请输入文章标题..." type="text"/>
-                <div className="submit">发布</div>
+                <input className="title-input" ref={el=>this.title = el} placeholder="请输入文章标题..." type="text"/>
+                <div className="submit" onClick={this.submitArticle}>发布</div>
             </div>
             <div className="box">
                 <div id="editor">
-                    <textarea style={{"display":"none"}}></textarea>
+                    <textarea ref={el=>this.textarea = el} style={{"display":"none"}}></textarea>
                 </div>
             </div>
             
@@ -28,5 +29,29 @@ export default class Index extends Component{
                 return ["undo","redo","|","bold","del","italic","quote","ucwords","uppercase","lowercase","|","h1","h2","h3","h4","h5","h6","|","list-ul","list-ol","hr","|","link","image","code","preformatted-text","code-block","table","datetime","html-entities","|","clear"]
             }
         });
+    }
+    submitArticle = ()=>{
+        if(!this.title.value){
+            alert('标题不能为空！');
+            return;
+        }
+        if(!this.textarea.value){
+            alert('文章内容不能不能为空！');
+            return;
+        }
+
+        axios.post("/api/publishArticle",{
+            title:this.title.value,
+            content:this.textarea.value
+        },{
+            headers:{
+                "Content-Type":"text/plain; charset=utf-8",
+                "withCredentials":true
+            }
+        },(data)=>{
+            console.log(data)
+        });
+
+        
     }
 }
