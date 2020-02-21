4c70abbba646fd518d77a896c901885adab9c7bc
陈泽 <18815288453@163.com>
2020-02-21 17:32:03 +0800

    新增倒计时页面

---
+++ pages/checkTime/checkTime.jsx
@@ -0,0 +1,71 @@
+import React,{ Component } from 'react';
+import "./checkTime.less";
+
+export default class ChecTime extends Component{
+    render(){
+        let {holidayList,nowTime} = this.state;
+
+        return <div className="check-time-page">
+            {this.getCurrentTime().map((item,index)=>{
+                let times = new Date(item.time).getTime()-nowTime;
+                if(times<0){
+                    return null
+                }
+                let hours = parseInt(times/1000/60/60);
+                let minites = parseInt((times-hours*60*60*1000)/1000/60);
+                let seconds = parseInt((times-hours*60*60*1000-minites*60*1000)/1000);
+                let minSeconds = times-hours*60*60*1000-minites*60*1000-seconds*1000
+                return <div className="litm-list" key={index}>
+                    <div>{item.name}倒计时</div>
+                    <div>
+                        <div className="time-box">{hours}</div>:<div className="time-box">{minites}</div>:<div className="time-box">{seconds}</div>:<div className="time-box">{minSeconds}</div>
+                    </div>
+                </div>
+            })}
+        </div>
+    }
+    constructor(){
+        super();
+        this.state = {
+            holidayList:[{
+                name:"五一",
+                time:"2020-05-01"
+            },{
+                name:"国庆",
+                time:"2020-10-01"
+            },{
+                name:"元旦",
+                time:"2021-01-01"
+            },{
+                name:"除夕",
+                time:"2021-02-11"
+            }],
+            nowTime:Date.now()
+        }
+    }
+
+    componentDidMount(){
+        this.cicle();
+    }
+    cicle = ()=>{
+        setTimeout(()=>{
+            this.setState({
+                nowTime:Date.now()
+            },()=>{
+                this.cicle()
+            });
+        },10)
+    }
+
+    getCurrentTime = ()=>{
+        let {holidayList,nowTime} = this.state;
+        let currentTime = [];
+        holidayList.forEach((item)=>{
+            if(new Date(item.time).getTime()-nowTime>0&&currentTime.length===0){
+                currentTime = [item]
+            }
+        });
+
+        return currentTime;
+    }
+}
